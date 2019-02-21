8df50573c59fcc54ab40b8687bb3f3ef7a36b404
rasm547h <rasmuslarsson2010@hotmail.com>
2019-02-21 15:26:50 +0100

    Old package module removed. Loggin added to censor function.

--- censortext.js
+++ censortext.js
@@ -5,6 +5,7 @@ function censor(inStr) {
 		inStr = inStr.replace(censoredWords[idx], "****");
 	}
 	for (idx in customCensoredWords) {
+		console.log(customCensoredWords[idx]);
 		inStr = inStr.replace(customCensoredWords[idx], "****");
 	}
 	return inStr;
